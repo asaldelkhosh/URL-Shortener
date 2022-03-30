551648c2da6e45519c34f4c92caadbfbf93f42d0
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-30 16:07:08 +0430

    peer connection config init

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -1,5 +1,18 @@
 package cmd
 
-func Exec() {
+import (
+	"fmt"
+	"github.com/pion/webrtc/v2"
+)
 
+func Exec() {
+	peerConnectionConfig := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
+
+	fmt.Println(peerConnectionConfig.Certificates)
 }

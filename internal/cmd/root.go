f2ac802990a9a666cd99a912cf7586febdf4216b
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-30 18:19:48 +0430

    peer connection config added in media

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -2,18 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/amirhnajafiz/Blue-sky/internal/pion/media"
 
 	"github.com/pion/webrtc/v2"
 )
 
 func Exec() {
-	peerConnectionConfig := webrtc.Configuration{
-		ICEServers: []webrtc.ICEServer{
-			{
-				URLs: []string{"stun:stun.l.google.com:19302"},
-			},
-		},
-	}
+	peerConnectionConfig := media.GetPeerConfig()
 
 	fmt.Println(peerConnectionConfig.Certificates)
 
---
+++ internal/pion/media/config.go
@@ -0,0 +1,13 @@
+package media
+
+import "github.com/pion/webrtc/v2"
+
+func GetPeerConfig() webrtc.Configuration {
+	return webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
+}

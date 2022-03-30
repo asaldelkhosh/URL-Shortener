26fb07dd40f5c40a8eb46e11cfb992407bff37df
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-30 18:21:28 +0430

    media engine package added

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -2,19 +2,14 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/amirhnajafiz/Blue-sky/internal/pion/media"
 
-	"github.com/pion/webrtc/v2"
+	"github.com/amirhnajafiz/Blue-sky/internal/pion/media"
 )
 
 func Exec() {
 	peerConnectionConfig := media.GetPeerConfig()
+	api := media.GetMediaAPI()
 
 	fmt.Println(peerConnectionConfig.Certificates)
-
-	m := webrtc.MediaEngine{}
-	m.RegisterCodec(webrtc.NewRTPVP8Codec(webrtc.DefaultPayloadTypeVP8, 90000))
-	api := webrtc.NewAPI(webrtc.WithMediaEngine(m))
-
 	fmt.Println(api)
 }
--- internal/pion/media/media.go
+++ internal/pion/media/media.go
@@ -1,1 +1,11 @@
 package media
+
+import "github.com/pion/webrtc/v2"
+
+func GetMediaAPI() *webrtc.API {
+	m := webrtc.MediaEngine{}
+
+	m.RegisterCodec(webrtc.NewRTPVP8Codec(webrtc.DefaultPayloadTypeVP8, 90000))
+
+	return webrtc.NewAPI(webrtc.WithMediaEngine(m))
+}

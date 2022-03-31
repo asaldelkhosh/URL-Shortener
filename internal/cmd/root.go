d2bd8d3b1f469c4d954282d7247ebe7315d15199
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-31 09:37:27 +0430

    peerconnection in root

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -1,15 +1,30 @@
 package cmd
 
 import (
-	"fmt"
+	"github.com/pion/webrtc/v2"
 
 	"github.com/amirhnajafiz/Blue-sky/internal/pion/media"
 )
+
+type Sdp struct {
+	Sdp string
+}
 
 func Exec() {
 	peerConnectionConfig := media.GetPeerConfig()
 	api := media.GetMediaAPI()
 
-	fmt.Println(peerConnectionConfig.Certificates)
-	fmt.Println(api)
+	var session Sdp
+	offer := webrtc.SessionDescription{}
+
+	peerConnection, _ := api.NewPeerConnection(peerConnectionConfig)
+
+	_ = peerConnection.SetRemoteDescription(offer)
+
+	answer, _ := peerConnection.CreateAnswer(nil)
+
+	err := peerConnection.SetLocalDescription(answer)
+	if err != nil {
+		panic(err)
+	}
 }

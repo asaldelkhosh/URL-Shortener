ba59954c27c6035ef8e9c8adf7c78f0641fa9582
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-02 10:08:47 +0430

    signal init

--- internal/pion/signal/signal.go
+++ internal/pion/signal/signal.go
@@ -1,1 +1,35 @@
 package signal
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// Allows compressing offer/answer to bypass terminal input limits.
+const compress = false
+
+func MustReadStdin() string {
+	r := bufio.NewReader(os.Stdin)
+
+	var in string
+	for {
+		var err error
+		in, err = r.ReadString('\n')
+		if err != io.EOF {
+			if err != nil {
+				panic(err)
+			}
+		}
+		in = strings.TrimSpace(in)
+		if len(in) > 0 {
+			break
+		}
+	}
+
+	fmt.Println("")
+
+	return in
+}

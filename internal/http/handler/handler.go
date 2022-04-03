02d4ebd2ce2b3a6a83c4071c625caea7213f484e
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-03 15:25:47 +0430

    handler is good

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -16,32 +16,42 @@ type Sdp struct {
 	Sdp string
 }
 
-func Register(cfg config.Config, app *gin.RouterGroup) {
-	peerConnectionMap := make(map[string]chan *webrtc.Track)
-	api := media.GetMediaAPI()
-	peerConnectionConfig := media.GetPeerConfig()
+type Handler struct {
+	Cfg                  config.Config
+	PeerConnectionMap    map[string]chan *webrtc.Track
+	Api                  *webrtc.API
+	PeerConnectionConfig webrtc.Configuration
+}
 
-	app.POST("/webrtc/sdp/m/:meetingId/c/:userID/p/:peerId/s/:isSender", func(c *gin.Context) {
-		isSender, _ := strconv.ParseBool(c.Param("isSender"))
-		userID := c.Param("userID")
-		peerID := c.Param("peerId")
+func (h *Handler) Call(c *gin.Context) {
+	isSender, _ := strconv.ParseBool(c.Param("isSender"))
+	userID := c.Param("userID")
+	peerID := c.Param("peerId")
 
-		var session Sdp
-		if err := c.ShouldBindJSON(&session); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+	var session Sdp
+	if err := c.ShouldBindJSON(&session); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-		offer := webrtc.SessionDescription{}
-		signal.Decode(cfg.Signal.Compress, session.Sdp, &offer)
+	offer := webrtc.SessionDescription{}
+	signal.Decode(h.Cfg.Signal.Compress, session.Sdp, &offer)
 
-		peerConnection, _ := api.NewPeerConnection(peerConnectionConfig)
-		_ = peerConnection.SetRemoteDescription(offer)
+	peerConnection, _ := h.Api.NewPeerConnection(h.PeerConnectionConfig)
+	_ = peerConnection.SetRemoteDescription(offer)
 
-		if !isSender {
-			track.ReceiveTrack(peerConnection, peerConnectionMap, peerID)
-		} else {
-			track.CreateTrack(peerConnection, peerConnectionMap, userID)
-		}
-	})
+	if !isSender {
+		track.ReceiveTrack(peerConnection, h.PeerConnectionMap, peerID)
+	} else {
+		track.CreateTrack(peerConnection, h.PeerConnectionMap, userID)
+	}
+}
+
+func (h *Handler) Register(cfg config.Config, app *gin.RouterGroup) {
+	h.Cfg = cfg
+	h.PeerConnectionMap = make(map[string]chan *webrtc.Track)
+	h.Api = media.GetMediaAPI()
+	h.PeerConnectionConfig = media.GetPeerConfig()
+
+	app.POST("/webrtc/sdp/m/:meetingId/c/:userID/p/:peerId/s/:isSender", h.Call)
 }

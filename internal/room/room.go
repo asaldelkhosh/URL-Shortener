7f809c28241e5ff4535f7cb6e16b9b0b8d20930b
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-03 14:38:39 +0430

    room methods implemented

--- internal/room/room.go
+++ internal/room/room.go
@@ -1,4 +1,15 @@
 package room
 
+import "github.com/pion/webrtc/v2"
+
 type Room struct {
+	Id                string
+	PeerConnectionMap map[string]chan *webrtc.Track
+}
+
+func New(id string) Room {
+	return Room{
+		Id:                id,
+		PeerConnectionMap: make(map[string]chan *webrtc.Track),
+	}
 }

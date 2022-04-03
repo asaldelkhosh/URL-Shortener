bbd75ebba8f4d3ecf0bd4f4ea9fd3271c0f9bb31
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-03 14:41:48 +0430

    add available field for rooms

--- internal/room/room.go
+++ internal/room/room.go
@@ -2,14 +2,22 @@ package room
 
 import "github.com/pion/webrtc/v2"
 
+var rooms []Room
+
 type Room struct {
 	Id                string
+	Available         bool
 	PeerConnectionMap map[string]chan *webrtc.Track
 }
 
 func New(id string) Room {
-	return Room{
+	room := Room{
 		Id:                id,
+		Available:         true,
 		PeerConnectionMap: make(map[string]chan *webrtc.Track),
 	}
+
+	rooms = append(rooms, room)
+
+	return room
 }

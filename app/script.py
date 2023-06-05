645ae0496b417629ea671aa6f12972b863a1ea38
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-05 13:55:05 +0330

    add: flask init

--- app/script.py
+++ app/script.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+from flask import Flask
 
 from query import Query
 
@@ -28,3 +29,31 @@ print('[INFO] connection stablished')
 if migrate:
   dbConnection.execute(queryParser.createTable())
   print('[INFO] migration successed')
+
+
+# create a new flask app
+app = Flask(__name__)
+
+
+
+# creating http routes
+
+@app.route("/") # return the home page
+def index():
+  pass
+
+@app.route("/url", methods=['GET']) # return all of the urls
+def getURLs():
+  pass
+
+@app.route("/url", methods=['POST']) # create a new url
+def createURL():
+  pass
+
+@app.route("/url/<id>", methods=['POST']) # remove an url
+def deleteURL():
+  pass
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=3000)

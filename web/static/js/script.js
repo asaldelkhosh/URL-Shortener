c9f3997a680864ee1a012d1e278e568cde1efbba
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-05 14:27:25 +0330

    update: create url script

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -1,6 +1,27 @@
+const host = '127.0.0.1:3000';
+
+
+
 // create a new url by executing a fetch
 function createURL() {
+    let data = document.getElementById('url-input').value
 
+    fetch(host+"/url",
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({'url': data})
+        })
+        .then(() => {
+            window.location.reload()
+        })
+        .catch((error) => {
+            console.error(error)
+            alert('Failed to create URL!')
+        })
 }
 
 // remove a url by executing a fetch
